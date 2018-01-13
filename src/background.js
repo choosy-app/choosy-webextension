88cfcabada1c196e01d9a70b3054599ed8c9f2fa
George Brocklehurst <george@georgebrock.com>
2018-01-12 22:22:26 -0500

    Fix Firefox Quantum compatibility.
    Recent versions of Firefox, possibly but not definitely aligned with the
    release of Quantum, haven't been working with the Choosy extension.
    
    The root of the problem is that creating an iframe with an `x-choosy` URL in
    the background script no longer triggers Choosy from Firefox.
    
    The alternative was to open a new tab with the `x-choosy` URL, and
    immediately close it again.
    
    Unfortunately, the new Firefox code does not work in Google Chrome. The tab
    opening callback fires too early, and even adding a delay (ranging from 0 to
    100 ms) doesn't give Chrome time to activate Choosy. In the meantime, the
    tab opening animation is quite jarring.
    
    This commit therefore introduces branching logic for how we define the
    function that opens Choosy. So much of the code is now separate branches,
    that I question the usefulness of a single repository. It might be useful to
    split them up again in future.

--- src/background.js
+++ src/background.js
@@ -1,22 +1,39 @@
+var callChoosy;
+
 if (typeof browser === "undefined") {
+  // Google Chrome
   browser = chrome;
+  callChoosy = function (url, sourceTab) {
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute("src", url);
+    document.body.appendChild(iframe);
+  };
+} else {
+  // Mozilla Firefox
+  callChoosy = function (url, sourceTab) {
+    browser.tabs.create({
+      "url": url,
+      "openerTabId": sourceTab.id,
+      "active": false
+    }).then(function (tab) {
+      browser.tabs.remove(tab.id);
+    });
+  };
 }
 
 var Choosy = {
-  promptAll: function(url) {
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "x-choosy://prompt.all/" + escape(url));
-    document.body.appendChild(iframe);
+  promptAll: function(url, sourceTab) {
+    callChoosy("x-choosy://prompt.all/" + escape(url), sourceTab);
   }
 };
 
-browser.browserAction.onClicked.addListener(function(tab) {
-  Choosy.promptAll(tab.url);
+browser.browserAction.onClicked.addListener(function (tab) {
+  Choosy.promptAll(tab.url, tab);
 });
 
 browser.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "choosy-menu-item") {
-    Choosy.promptAll(info.linkUrl);
+    Choosy.promptAll(info.linkUrl, tab);
   }
 });
 
