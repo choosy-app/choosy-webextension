b004c9ce76c556b2d439c65f33d9aa79a55014a3
George Brocklehurst <george@georgebrock.com>
2020-06-20 16:27:54 -0400

    Change JavaScript class definition.
    This commit changes the class definition to use methods instead of arrow
    functions assigned to class field properties. The prior version of the code
    worked in Firefox, but was flagged as a syntax error in Mozilla's Firefox
    add-on submission tool.

--- src/background.js
+++ src/background.js
@@ -1,38 +1,46 @@
 "use strict";
 
 class Choosy {
-  promptAll = ({tab, ...request}) => this.call({
-    method: "prompt.all",
-    tab,
-    url: tab.url,
-    ...request,
-  })
+  promptAll({tab, ...request}) {
+    this.call({
+      method: "prompt.all",
+      tab,
+      url: tab.url,
+      ...request,
+    });
+  }
 
-  call = (request) => this.nativeMessagingCall(request).catch((error) => {
-    console.error("Native messaging failed: ", error);
-    this.legacyCall(request);
-  })
+  call(request) {
+    this.nativeMessagingCall(request).catch((error) => {
+      console.error("Native messaging failed: ", error);
+      this.legacyCall(request);
+    });
+  }
 
-  nativeMessagingCall = ({method, url}) => new Promise((resolve, reject) => {
-    browser.runtime.sendNativeMessage(
-      "com.choosyosx.choosy.nativemessaging",
-      {method, url},
-      (response) => {
-        if (!response) {
-          reject(browser.runtime.lastError.message);
-        } else if (response && !response.ok) {
-          reject(response);
-        } else {
-          resolve();
+  nativeMessagingCall({method, url}) {
+    new Promise((resolve, reject) => {
+      browser.runtime.sendNativeMessage(
+        "com.choosyosx.choosy.nativemessaging",
+        {method, url},
+        (response) => {
+          if (!response) {
+            reject(browser.runtime.lastError.message);
+          } else if (response && !response.ok) {
+            reject(response);
+          } else {
+            resolve();
+          }
         }
-      }
-    );
-  })
+      );
+    });
+  }
 
-  legacyCall = ({method, url, tab}) => browser.tabs.update(
-    tab.id,
-    {url: "x-choosy://" + method + "/" + url},
-  )
+  legacyCall({method, url, tab}) {
+    browser.tabs.update(
+      tab.id,
+      {url: "x-choosy://" + method + "/" + url},
+    );
+  }
 };
 
 const choosy = new Choosy();
